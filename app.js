1d07fec44c01f36e35709ea38399b865264fd464
Syed Muhammad Arham <syedarhamreal@gmail.com>
2024-03-30 16:23:55 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// run any one at a time
+
+var age = +prompt("what is your age");
+var gender = prompt("what is gender");
+
+if (age > 18 || gender == "male") {
+  alert("you are  eligible");
+} else {
+  alert("you are not aligible");
+}
+
+
+
+var x = 5;
+var y = 10;
+var a = 30;
+var b = 15;
+if (x + y == a - b) {
+  alert("correct");
+} else {
+  alert("incorrect");
+}
